f7dbdc22800e5bbf87332e3bbfdec86f72f5a76f
Eric Shull <eric@exupero.org>
2016-10-21 12:50:43 -0400

    Play the game.

--- clojure/turtles/src/turtles/core.clj
+++ clojure/turtles/src/turtles/core.clj
@@ -5,10 +5,22 @@
   (println x)
   x)
 
+(defn roll-die []
+  (inc (rand-int 6)))
+
 (def all-players [:red :blue :green :purple])
 
 (defn make-player [color] 
-  {:color color :tokens 0 :skip-next-turn false :tile :tile1 :has-won false :lost-turns 0})
+  {:color color
+   :tokens 0
+   :skip-next-turn false
+   :tile :tile1
+   :has-won false
+   :lost-turns 0
+   :take-shortcut (fn [player]
+                    (or (= :purple color)
+                        (and (= :green color)
+                             (<= 18 (:tokens player)))))})
 
 (defn make-tile 
   ([] (make-tile :empty 0))
@@ -51,6 +63,20 @@
 (defmethod forward-from :shortcut-option [game tile player n]
   (assoc-in game [:players player :tile] tile))
 
+(defmethod forward-from :steal [game tile player n]
+  (if (zero? n)
+    (let [target-player (apply max-key :tokens (-> game :players (dissoc player) vals))
+          roll (roll-die)
+          takable (min roll (:tokens target-player))]
+      (-> game
+        (assoc-in [:players player :tile] tile)
+        (update-in [:players player :tokens] + takable)
+        (update-in [:players player :took] conj takable)
+        (update-in [:players (:color target-player) :tokens] - takable)
+        (update-in [:players (:color target-player) :taken] conj takable)))
+    (let [next-tile (first (get-in game [:board :graph tile]))]
+      (forward-from game next-tile player (dec n)))))
+
 (defmethod forward-from :start-finish [game tile player n]
   (if (> (get-in game [:players player :tokens]) 10)
     (assoc-in game [:players player :has-won] true)
@@ -76,20 +102,20 @@
            :tile13 [:tile14]
            :tile14 [:tile15]
            :tile15 [:tile16]
-           :tile16 [:tile17]
+           :tile16 [:tile17, :tile22]
            :tile17 [:tile18]
            :tile18 [:tile19]
            :tile19 [:tile20]
-           :tile20 [:tile21, :tile29]
+           :tile20 [:tile21]
            :tile21 [:tile22]
            :tile22 [:tile23]
            :tile23 [:tile24]
            :tile24 [:tile25]
            :tile25 [:tile26]
-           :tile26 [:tile27]
-           :tile27 [:tile28]
-           :tile28 [:tile29]
-           :tile29 [:tile1]
+           :tile26 [:tile1]
+           ; :tile27 [:tile28]
+           ; :tile28 [:tile29]
+           ; :tile29 [:tile1]
            } 
    :tiles {
            :tile1 (make-tile :start-finish) 
@@ -104,15 +130,15 @@
            :tile10 (make-tile)
            :tile11 (make-tile :lose-a-turn)
            :tile12 (make-tile)
-           :tile13 (make-tile)
+           :tile13 (make-tile :steal)
            :tile14 (make-tile :token-change 2)
            :tile15 (make-tile)
-           :tile16 (make-tile)
+           :tile16 (make-tile :shortcut-option 8)
            :tile17 (make-tile :lose-a-turn)
            :tile18 (make-tile)
            :tile19 (make-tile :token-change 3)
-           :tile20 (make-tile :shortcut-option 5)
-           :tile21 (make-tile)
+           :tile20 (make-tile)
+           :tile21 (make-tile :steal)
            :tile22 (make-tile)
            :tile23 (make-tile :token-change -1)
            :tile24 (make-tile)
@@ -124,22 +150,30 @@
            }})
 
 (defn make-game []
-  (let [the-players (make-players 2)] 
-    {:board (make-board) :players (zipmap (map :color the-players) the-players) :turn 0 :next-players (cycle (map :color the-players))}))
+  (let [the-players (make-players 4)] 
+    {:board (make-board)
+     :players (zipmap (map :color the-players) the-players)
+     :turn 0
+     :next-players (cycle (map :color the-players))}))
 
 (defn display-game [game]
   (pprint {:turn (:turn game) :players (:players game)}))
 
-(defn roll-die []
-  (inc (rand-int 6)))
-
 (defn move-player [game current-player moves] 
   (let [tile (get-in game [:players current-player :tile])
-        next-tile (first (get-in game [:board :graph tile]))]
-    (forward-from game
-                  next-tile
-                  current-player
-                  (dec moves))))
+        next-tile (first (get-in game [:board :graph tile]))
+        player (get-in game [:players current-player])]
+    (if (and (= :shortcut-option (:tile-type tile))
+             (:take-shortcut player)
+             (<= (:value tile) (:tokens player)))
+      (forward-from (update-in game [:players current-player :tokens] - (:value tile))
+                    (last (get-in game [:board :graph tile]))
+                    current-player
+                    (dec moves))
+      (forward-from game
+                    next-tile
+                    current-player
+                    (dec moves)))))
 
 (defn roll-and-move [game current-player] 
   (move-player game current-player (roll-die)))
@@ -156,7 +190,7 @@
   (find-first :has-won (vals (:players game))))
 
 (defn has-winner? [game]
-  (some true? (map :has-won (vals (:players game)))))
+  (some :has-won (vals (:players game))))
 
 (defn play-turn [game] 
   (let [ current-player (first (:next-players game)) ]
@@ -185,7 +219,3 @@
   ;; (display-game (first (take 5 (map play-game)))))
   ;; (display-game (last (repeatedly 10000 #(play-game)))))
   (spy (float (avg (map :turn (repeatedly 100 #(play-game)))))))
-
-(comment
-  (-main)
-  )
